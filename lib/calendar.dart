7bd5658ac31842977a56e9b2d337b6a0eb98fc0f
Alex Musick <97472676+alex-musick@users.noreply.github.com>
2026-03-11 18:07:31 -0500

    Preparing for calendar integration

---
+++ lib/calendar.dart
@@ -0,0 +1,10 @@
+import 'package:googleapis/calendar/v3.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+import 'habitscreen.dart';
+import 'authscreen.dart';
+import 'historyscreen.dart';
+import 'habitbuilder.dart';
+import 'habit.dart';
+import 'package:provider/provider.dart';
+
